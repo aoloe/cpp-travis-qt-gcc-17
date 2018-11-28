787c4561cee27ad4b2502b66a9dc73162631a6d3
ale rimoldi <ale@graphicslab.org>
2018-11-28 10:49:41 +0100

    and finally a qt app with a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 
-#include <QCoreApplication>
-#include <QTimer>
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
 #include <QDebug>
 
 int main(int argc, char *argv[]) 
 {
-    static_assert("C++17");
-    std::cout << "Hello world\n";
-
-    QCoreApplication app(argc, argv);
-
-    QTimer::singleShot( 0, &app, &QCoreApplication::quit );
-    
-    qDebug() << "Hello Qt-World";
-
+    QApplication app(argc, argv);
+    QMainWindow mainWindow;
+    QLabel *label = new QLabel(&mainWindow);
+    label->setText("Hello Qt World");
+    mainWindow.show();
     return app.exec();
 }
