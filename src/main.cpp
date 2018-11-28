53fb3038003b1b6afc65ba42ea01620b39f010e3
ale rimoldi <ale@graphicslab.org>
2018-11-28 09:57:09 +0100

    add qt to the mix

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int main() 
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[]) 
 {
-  static_assert("C++17");
-  std::cout << "Hello world\n";
+    static_assert("C++17");
+    std::cout << "Hello world\n";
+
+    QCoreApplication a(argc, argv);
+    
+    qDebug() << "Hello World";
+
+    return a.exec();
 }
